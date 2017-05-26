c4d89c9d301d6e66515e5b8776900bb7df4ce2c0
kiburei <akiburei@gmail.com>
2017-05-26 17:09:08 +0300

    add required gulp tasks for optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
-// Change code to browser readable format
 var browserify = require('browserify');
-// put browserified files into one file
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+
+var buildProduction = utilities.env.production;
 
 
 gulp.task('jsBrowserify', function() {
@@ -10,4 +13,22 @@ gulp.task('jsBrowserify', function() {
 	.bundle()
 	.pipe(source('app.js'))
 	.pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function() {
+	return gulp.src("./build/js/app.js")
+	.pipe(uglify())
+	.pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,11 +1,21 @@
+var GitUser = require('./../js/git_requests.js').getRepos;
+
+$(document).ready(function(){
+	// event.preventDefault();
+	$("#gitForm").submit(function(event){
+		event.preventDefault();
+		var user = $("#gitUser").val();
+		// console.log(user);
+		var giter = GitUser(user);
+		console.log(giter);
+	});
+});
 var apiKey = require('./../.env').apiKey;
 
 	exports.getRepos = function(user){
 		// var apiKey = "582040ffa017dc95fac9fb9d7f941f9c0c0f9887"
 	  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
 	    console.log(response);
-	    $("#name").text(response.login + " has " + response.public_repos + ". You can view them at " + response.repos_url +". For more info check out user at ");
-	    $("#desc").text(response.url);
 	  }).fail(function(error){
 	    console.log(error.responseJSON.message);
 	  });
