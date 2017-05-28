693a0dbd4890d078a2b3debcf5d609b4529c6d5d
kiburei <akiburei@gmail.com>
2017-05-28 14:05:58 +0300

    more css on sterids

--- build/js/app.js
+++ build/js/app.js
@@ -3,6 +3,7 @@ exports.apiKey = "582040ffa017dc95fac9fb9d7f941f9c0c0f9887"
 
 },{}],2:[function(require,module,exports){
 var GitUser = require('./../js/git_requests.js').getRepos;
+var moment = require()
 
 $(document).ready(function(){
 	$("#gitForm").submit(function(event){
@@ -11,6 +12,7 @@ $(document).ready(function(){
 		GitUser(user);
 		// console.log(userD);
 	});
+	$('#time').text(moment());
 });
 },{"./../js/git_requests.js":3}],3:[function(require,module,exports){
 var apiKey = require('./../.env').apiKey;
@@ -19,8 +21,7 @@ var apiKey = require('./../.env').apiKey;
 		// var apiKey = "582040ffa017dc95fac9fb9d7f941f9c0c0f9887"
 	  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
 	    console.log(response);
-	    $("#name").text(response.login + " has " + response.public_repos + ". You can view them at " + response.repos_url +". For more info check out user at ");
-	    $("#desc").text(response.url);
+	    $("#desc").text(response.login + " has " + response.public_repos + ". You can view them at " + response.repos_url +". For more info check out user at " + response.url + ".");
 	  }).fail(function(error){
 	    console.log(error.responseJSON.message);
 	  });
--- js/git_requests.js
+++ js/git_requests.js
@@ -4,8 +4,7 @@ var apiKey = require('./../.env').apiKey;
 		// var apiKey = "582040ffa017dc95fac9fb9d7f941f9c0c0f9887"
 	  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
 	    console.log(response);
-	    $("#name").text(response.login + " has " + response.public_repos + ". You can view them at " + response.repos_url +". For more info check out user at ");
-	    $("#desc").text(response.url);
+	    $("#desc").text(response.login + " has " + response.public_repos + ". You can view them at " + response.repos_url +". For more info check out user at " + response.url + ".");
 	  }).fail(function(error){
 	    console.log(error.responseJSON.message);
 	  });
